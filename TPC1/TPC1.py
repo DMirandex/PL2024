fc6b8cc1da1c50491ccfe8d9bf3b7464dac5eaf2
DMirandex <125562188+DMirandex@users.noreply.github.com>
2024-02-16 09:17:43 +0000

    Add files via upload

--- TPC1/TPC1.py
+++ TPC1/TPC1.py
@@ -8,50 +8,77 @@ class AnalisadorCSV:
         header = dataset.pop(0)
 
         # Variáveis para as estatísticas
-        modalidades = set()
-        aptos = 0
-        inaptos = 0
-        idades = []
+        self.modalidades = set()
+        self.aptos = 0
+        self.inaptos = 0
+        self.idades = []
 
         # 0    1      2         3          4           5     6      7        8        9    10      11      12
         #_id,index,dataEMD,nome/primeiro,nome/último,idade,género,morada,modalidade,clube,email,federado,resultado
         for line in dataset:
-            modalidades.add(line[8])
+            self.modalidades.add(line[8])
 
             # Verificando se o atleta é apto ou inapto
             if line[12] == 'true':
-                aptos += 1
+                self.aptos += 1
             else:
-                inaptos += 1
+                self.inaptos += 1
 
             # Recolha das idades para a distribuição etária
             idade = int(line[5])  # Considerando idade mínima de 30 anos
-            idades.append(idade)
-
-        # Calculando percentagens de aptos e inaptos
-        total_atletas = len(dataset)
-        percent_aptos = (aptos / total_atletas) * 100
-        percent_inaptos = (inaptos / total_atletas) * 100
+            self.idades.append(idade)
 
         # Ordenando modalidades alfabeticamente
-        modalidades_ordenadas = sorted(modalidades)
+        self.modalidades_ordenadas = sorted(self.modalidades)
 
         # Criando distribuição de atletas por escalão etário
-        idades.sort()
-        escaloes = {}
-        for idade in idades:
-            if idade >= 30:
-                escalao = (idade // 5) * 5
-                escaloes[escalao] = escaloes.get(escalao, 0) + 1
+        self.idades.sort()
+        self.escaloes_etarios = {}
+        for idade in self.idades:
+            # Encontrando o intervalo etário
+            escalao_inicio = (idade // 5) * 5
+            escalao_fim = escalao_inicio + 4
+            escalao = f"[{escalao_inicio}-{escalao_fim}]"
+            self.escaloes_etarios[escalao] = self.escaloes_etarios.get(escalao, 0) + 1
 
-        self.modalidades_ordenadas = modalidades_ordenadas
-        self.percent_aptos = percent_aptos
-        self.percent_inaptos = percent_inaptos
-        self.escaloes_etarios = escaloes
+        # Removendo intervalos sem atletas
+        self.escaloes_etarios = {k: v for k, v in self.escaloes_etarios.items() if v > 0}
+
+    def mostrar_modalidades(self):
+        print("Modalidades:", self.modalidades_ordenadas)
+
+    def mostrar_percentagens(self):
+        print("Percentagem de Aptos:", (self.aptos / (self.aptos + self.inaptos)) * 100)
+        print("Percentagem de Inaptos:", (self.inaptos / (self.aptos + self.inaptos)) * 100)
+
+    def mostrar_distribuicao_idade(self):
+        print("Distribuição de Idade:", self.escaloes_etarios)
+
+
+# Função para exibir o menu
+def exibir_menu():
+    print("Menu:")
+    print("1. Lista ordenada alfabeticamente das modalidades desportivas")
+    print("2. Percentagens de atletas aptos e inaptos para a prática desportiva")
+    print("3. Distribuição de atletas por escalão etário")
+    print("4. Sair")
+
 
 # Exemplo de uso
-analise = AnalisadorCSV('emd.csv')
-print("Modalidades:", analise.modalidades_ordenadas)
-print("Percentagem de Aptos:", analise.percent_aptos)
-print("Percentagem de Inaptos:", analise.percent_inaptos)
-print("Distribuição de Idade:", analise.escaloes_etarios)
+if __name__ == "__main__":
+    analise = AnalisadorCSV('emd.csv')
+    while True:
+        exibir_menu()
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            analise.mostrar_modalidades()
+        elif opcao == "2":
+            analise.mostrar_percentagens()
+        elif opcao == "3":
+            analise.mostrar_distribuicao_idade()
+        elif opcao == "4":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
